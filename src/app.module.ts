7ab23fa8066d8cee1f86560d37d05187c9be4539
grapedge <grapedge@gmail.com>
2021-04-30 10:07:10 +0800

    根据项目设计划分模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,23 @@
 import { Module } from '@nestjs/common';
+import { DeviceModule } from './device/device.module';
+import { CabinetModule } from './cabinet/cabinet.module';
+import { SensorModule } from './sensor/sensor.module';
+import { ConfigModule } from './config/config.module';
+import { UserModule } from './user/user.module';
+import { MedicineModule } from './medicine/medicine.module';
+import { AlarmModule } from './alarm/alarm.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DeviceModule,
+    CabinetModule,
+    SensorModule,
+    ConfigModule,
+    UserModule,
+    MedicineModule,
+    AlarmModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
