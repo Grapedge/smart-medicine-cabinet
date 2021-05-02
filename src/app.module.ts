0dd2b9c217fb8fb190dfb5489df82fc140347617
grapedge <grapedge@gmail.com>
2021-05-02 11:32:23 +0800

    update: 完成数据库配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,33 @@ import { Module } from '@nestjs/common';
 import { DeviceModule } from './device/device.module';
 import { CabinetModule } from './cabinet/cabinet.module';
 import { SensorModule } from './sensor/sensor.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { MedicineModule } from './medicine/medicine.module';
 import { AlarmModule } from './alarm/alarm.module';
 import { AuthModule } from './auth/auth.module';
 import { configuration } from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
+import type { Configuration } from './config/configuration';
+import type { DatabaseConfig } from './config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigService],
+      useFactory: async (configService: ConfigService<Configuration>) => {
+        const databaseConfig = configService.get<DatabaseConfig>('database');
+        const { user, password, host, port, database } = databaseConfig;
+        const mongoDbUri = `mongodb://${user}:${password}@${host}:${port}/${database}`;
+        return {
+          uri: mongoDbUri,
+        };
+      },
+      inject: [ConfigService],
     }),
     DeviceModule,
     CabinetModule,
