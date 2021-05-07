1834f61cc1091ed16583b33e4e66dd1f13baf6c0
grapedge <grapedge@gmail.com>
2021-05-07 21:14:06 +0800

    登录API更新

--- src/auth/dto/sign-in.dto.ts
+++ src/auth/dto/sign-in.dto.ts
@@ -1,7 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsPhoneNumber, IsString } from 'class-validator';
 
 export class SignInDto {
   @IsPhoneNumber('CN')
+  @ApiProperty({
+    description: '用户手机号',
+    example: '13311112222',
+  })
   phone: string;
 
   @IsString()
